60e4c58b878aa3eb670ee2c1859d6ea68499fab4
Luis <s.ros.lui@gmail.com>
2021-01-13 17:08:07 +0100

    more style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,62 +2,71 @@
 <footer class="main-footer">
     <div class="container">
         <div class="colonne">
-            <!-- colonna 1 -->
-            <div class="colonna">
-                <div>
-                    <h4>DC COMICS</h4>
-                    <ul>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Comics</a></li>
-                        <li><a href="">Movies</a></li>
-                        <li><a href="">TV</a></li>
-                        <li><a href="">Games</a></li>
-                        <li><a href="">Videos</a></li>
-                        <li><a href="">News</a></li>
-                    </ul>
+
+            <!-- colonna sx -->
+            <div class="colonna_sx">
+                <!-- colonna 1 -->
+                <div class="colonna">
+                    <div>
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Comics</a></li>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="">TV</a></li>
+                            <li><a href="">Games</a></li>
+                            <li><a href="">Videos</a></li>
+                            <li><a href="">News</a></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li><a href="">Shop Dc</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                </div>
+            
+                <!-- colonna 2 -->
+                <div class="colonna">
+                    <div>
+                        <h4>DC</h4>
+                        <ul>
+                            <li><a href="">Terms Of Use</a></li>
+                            <li><a href="">Privacy Policy (New)</a></li>
+                            <li><a href="">Ad Choices</a></li>
+                            <li><a href="">Advertising</a></li>
+                            <li><a href="">Jobs</a></li>
+                            <li><a href="">Subscriptions</a></li>
+                            <li><a href="">Talent Workshop</a></li>
+                            <li><a href="">CPSC Certificates</a></li>
+                            <li><a href="">Ratings</a></li>
+                            <li><a href="">Shop Helps</a></li>
+                            <li><a href="">Contact Us</a></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
-                    <h4>SHOP</h4>
-                    <ul>
-                        <li><a href="">Shop Dc</a></li>
-                        <li><a href="">Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
-            </div>
-        
-            <!-- colonna 2 -->
-            <div class="colonna">
-                <div>
-                    <h4>DC</h4>
-                    <ul>
-                        <li><a href="">Terms Of Use</a></li>
-                        <li><a href="">Privacy Policy (New)</a></li>
-                        <li><a href="">Ad Choices</a></li>
-                        <li><a href="">Advertising</a></li>
-                        <li><a href="">Jobs</a></li>
-                        <li><a href="">Subscriptions</a></li>
-                        <li><a href="">Talent Workshop</a></li>
-                        <li><a href="">CPSC Certificates</a></li>
-                        <li><a href="">Ratings</a></li>
-                        <li><a href="">Shop Helps</a></li>
-                        <li><a href="">Contact Us</a></li>
-                    </ul>
+                <!-- colonna 3 -->
+                <div class="colonna">
+                    <div>
+                        <h4>SITES</h4>
+                        <ul>
+                            <li><a href="">DC</a></li>
+                            <li><a href="">MAD Magazine</a></li>
+                            <li><a href="">DC Kids</a></li>
+                            <li><a href="">DC Universe</a></li>
+                            <li><a href="">DC POwer Visa</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
-            <!-- colonna 3 -->
-            <div class="colonna">
-                <div>
-                    <h4>SITES</h4>
-                    <ul>
-                        <li><a href="">DC</a></li>
-                        <li><a href="">MAD Magazine</a></li>
-                        <li><a href="">DC Kids</a></li>
-                        <li><a href="">DC Universe</a></li>
-                        <li><a href="">DC POwer Visa</a></li>
-                    </ul>
-                </div>
+            <!-- colonna sx -->
+            <div class="colonna_dx">
+                <img src="{{asset('img/logo-footer.png')}}" alt="">
             </div>
         </div> 
 
